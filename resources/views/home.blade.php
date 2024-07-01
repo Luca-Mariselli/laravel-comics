f5c86afd0be36ecedbf16d08b4638fda37c2c5c4
Luca-Mariselli <lucamariselli@gmail.com>
2024-07-01 09:39:33 +0200

    creato store, aggiunto alle rotte e spostato array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
     <div>
-        @foreach ($arrayFilm as $film)
-            <li>{{$film['title']}}</li>
+        @foreach ($arrayFumetti as $fumetto)
+            <li>{{$fumetto['title']}}</li>
         @endforeach
     </div>
 </body>
